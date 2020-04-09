2281b16ccbd95cbe8d344ca833634706a314629c
Sofia Pineda <37553874+scpineda@users.noreply.github.com>
2020-04-09 13:33:57 -0700

    Add files via upload

--- lib/dice.dart
+++ lib/dice.dart
@@ -2,9 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'dart:math';
 
-class DiceRoller extends StatelessWidget{
-
+class DiceRoller extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,42 +13,27 @@ class DiceRoller extends StatelessWidget{
         home: Scaffold(
           body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              DiceTitle(),
-              instructions(),
-              DiceRoll()
-            ],
+            children: [DiceTitle(), instructions(), DiceRoll()],
           ),
-        )
-    );
+        ));
   }
 }
 
-Widget DiceTitle()
-{
+Widget DiceTitle() {
   return Container(
     margin: EdgeInsets.only(bottom: 50.0),
-    child: Text(
-        'Dice Roller',
+    child: Text('Dice Roller',
         textAlign: TextAlign.center,
-        style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 50.0)
-    ),
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50.0)),
   );
 }
 
-Widget instructions()
-{
+Widget instructions() {
   return Container(
     margin: const EdgeInsets.all(20.0),
-    child: Text(
-        'Enter the amount of dice, the die number, and any modifiers',
+    child: Text('Enter the amount of dice, the die number, and any modifiers',
         textAlign: TextAlign.center,
-        style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 25.0)
-    ),
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0)),
   );
 }
 
@@ -60,11 +45,11 @@ class DiceRoll extends StatefulWidget {
 
 // State class
 class _DiceRollState extends State<DiceRoll> {
-
   @override
   String _numOfDice = "";
   String _dieFaces = "";
   String _mod = "0";
+  String _result = "";
 
   Widget build(BuildContext context) {
     return Column(
@@ -72,48 +57,118 @@ class _DiceRollState extends State<DiceRoll> {
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            TextField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.number,
-              inputFormatters: [WhitelistingTextInputFormatter.digitsOnly], // Only numbers can be entered
-              onChanged: (String change)
-              {
-                setState(() {
-                  _numOfDice = change;
-                });
-              },
-            ),
+            Container(
+                margin: const EdgeInsets.all(20.0),
+                width: 75.0,
+                child: Center(
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(), labelText: "Num"),
+                        textAlign: TextAlign.center,
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          WhitelistingTextInputFormatter.digitsOnly
+                        ],
+                        onChanged: (String inp) {
+                          setState(() {
+                            _numOfDice = inp;
+                          });
+                        }))),
             Text(
-              'd'
+              'd',
+              style: TextStyle(fontSize: 35.0),
             ),
-            TextField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.number,
-              inputFormatters: [WhitelistingTextInputFormatter.digitsOnly], // Only numbers can be entered
-              onChanged: (String change)
-              {
-                setState(() {
-                  _dieFaces = change;
-                });
-              },
-            ),
-            Text(
-              '+'
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.number,
-              inputFormatters: [WhitelistingTextInputFormatter.digitsOnly], // Only numbers can be entered
-              onChanged: (String change)
-              {
-                setState(() {
-                  _mod = change;
-                });
-              },
-            )
+            Container(
+                margin: const EdgeInsets.all(20.0),
+                width: 75.0,
+                child: Center(
+                    child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Face"),
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+                  onChanged: (String inp) {
+                    setState(() {
+                      _dieFaces = inp;
+                    });
+                  },
+                ))),
+            Text('+', style: TextStyle(fontSize: 35)),
+            Container(
+                margin: const EdgeInsets.all(20.0),
+                width: 75.0,
+                child: Center(
+                    child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Mod"),
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+                  onChanged: (String inp) {
+                    setState(() {
+                      _mod = inp;
+                    });
+                  },
+                ))),
           ],
+        ),
+        RaisedButton(
+          color: Colors.blue,
+          onPressed: () {
+            setState(() {
+              _result = diceValue(_numOfDice, _dieFaces, _mod);
+            });
+          },
+          child: Text(
+            'Calculate',
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12.0),
+            border: Border.all(
+              color: Colors.blue,
+            ),
+          ),
+          margin: const EdgeInsets.all(10.0),
+          width: 90.0,
+          height: 90.0,
+          child: Center(
+            child: Text(
+              _result,
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 35.0,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
         )
       ],
     );
   }
+}
+
+String diceValue(String num, String face, String mod) {
+  // Convert everything into numbers
+  int intNum = int.parse(num);
+
+  int intFace = int.parse(face);
+
+  int intMod = int.parse(mod);
+
+  // generate a random number
+  var randomizer = new Random();
+  int result = 0;
+  var roll;
+
+  for (int i = 0; i < intNum; ++i) {
+    roll = randomizer.nextInt(intFace - 1) + 1;
+    result += roll;
+  }
+
+  result += intMod;
+
+  return result.toString();
 }
--- lib/hit.dart
+++ lib/hit.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class HitPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,56 +12,37 @@ class HitPage extends StatelessWidget {
         home: Scaffold(
           body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              HitTitle(),
-              instructions(),
-              HitCalc()
-            ],
+            children: [HitTitle(), instructions(), HitCalc()],
           ),
-        )
-    );
+        ));
   }
 }
 
-Widget HitTitle()
-{
+Widget HitTitle() {
   return Container(
     margin: EdgeInsets.only(bottom: 50.0),
-    child: Text(
-        'Hit Percentage',
+    child: Text('Hit Percentage',
         textAlign: TextAlign.center,
-        style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 50.0)
-    ),
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50.0)),
   );
 }
 
-Widget instructions()
-{
+Widget instructions() {
   return Container(
     margin: const EdgeInsets.all(20.0),
     child: Text(
         'Enter Enemy AC and Attack Modifier. Then, press the submit button.',
         textAlign: TextAlign.center,
-        style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 25.0)
-    ),
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0)),
   );
 }
 
-Widget resultText()
-{
+Widget resultText() {
   return Container(
     margin: const EdgeInsets.all(20.0),
-    child: Text(
-        'Your chance of hitting is...',
+    child: Text('Your chance of hitting is...',
         textAlign: TextAlign.center,
-        style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 25.0)
-    ),
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0)),
   );
 }
 
@@ -75,94 +55,86 @@ class HitCalc extends StatefulWidget {
 // State class
 class _HitCalcState extends State<HitCalc> {
   @override
-
   String _result = "";
   String _ac = "";
   String _hit = "";
 
-  Widget build(BuildContext context){
-    return Column(
-      children: <Widget> [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget> [
-            Container(
-              margin: const EdgeInsets.all(25.0),
-              width: 100.0,
-              child: Center(
-                child: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "AC"),
-                  textAlign: TextAlign.center,
-                  keyboardType: TextInputType.number,
-                  inputFormatters: [WhitelistingTextInputFormatter.digitsOnly], // Only numbers can be entered
-                  onChanged: (String acInp)
-                  {
-                    setState(() {
-                      _ac = acInp;
-                    });
-                  },
-                ),
-              ),
-            ),
-
-            Container(
-              margin: const EdgeInsets.all(25.0),
-              width: 100.0,
-              child: Center(
-                child: TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Hit"),
-                  textAlign: TextAlign.center,
-                  keyboardType: TextInputType.number,
-                  inputFormatters: [WhitelistingTextInputFormatter.digitsOnly] ,
-                    onChanged: (String acInp)
-                    {
-                      setState(() {
-                        _hit = acInp;
-                      });
-                    },// Only numbers can be entered
-                ),
-              ),
-            )
-          ]
-        ),
-        RaisedButton(
-          color: Colors.green,
-        onPressed: () {
-            setState(() {
-              _result = hitValue(_ac, _hit);
-            });
-        },
-          child: Text(
-            'Calculate',
-          ),
-        ),
-        resultText(),
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
         Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12.0),
-            border: Border.all(
-              color: Colors.green,
-            ),
-          ),
-          margin: const EdgeInsets.all(10.0),
-          width: 108.0,
-          height: 108.0,
+          margin: const EdgeInsets.all(25.0),
+          width: 100.0,
           child: Center(
-            child: Text(_result,
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 35.0,
-              ),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: "AC"),
               textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                WhitelistingTextInputFormatter.digitsOnly
+              ], // Only numbers can be entered
+              onChanged: (String acInp) {
+                setState(() {
+                  _ac = acInp;
+                });
+              },
             ),
           ),
         ),
-      ]
-    );
+        Container(
+          margin: const EdgeInsets.all(25.0),
+          width: 100.0,
+          child: Center(
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: "Hit"),
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+              onChanged: (String acInp) {
+                setState(() {
+                  _hit = acInp;
+                });
+              }, // Only numbers can be entered
+            ),
+          ),
+        )
+      ]),
+      RaisedButton(
+        color: Colors.green,
+        onPressed: () {
+          setState(() {
+            _result = hitValue(_ac, _hit);
+          });
+        },
+        child: Text(
+          'Calculate',
+        ),
+      ),
+      resultText(),
+      Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12.0),
+          border: Border.all(
+            color: Colors.green,
+          ),
+        ),
+        margin: const EdgeInsets.all(10.0),
+        width: 90.0,
+        height: 90.0,
+        child: Center(
+          child: Text(
+            _result,
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 35.0,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    ]);
   }
 }
 
